c9af2efdb8d4bff06eca5bcd63e4c6e68fa6865c
Yasser Nascimento <dev@yasser.email>
2026-02-17 10:00:09 -0300

    Update WebStreams typings

--- web.d.ts
+++ web.d.ts
@@ -1,7 +1,8 @@
-import { Writable } from '.'
-
 export interface ReadableStreamDefaultReader {
+  readonly closed: Promise<void>
+
   read(): Promise<{ value: unknown; done: boolean }>
+  releaseLock(): void
   cancel(reason?: unknown): Promise<void>
 }
 
@@ -24,6 +25,7 @@ export class ReadableStreamDefaultController {
 export interface UnderlyingSource<S extends ReadableStream = ReadableStream> {
   start?(this: S, controller: ReadableStreamDefaultController): void
   pull?(this: S, controller: ReadableStreamDefaultController): void
+  cancel?(this: S, reason?: unknown): void
 }
 
 export interface CustomQueuingStrategy {
@@ -32,9 +34,12 @@ export interface CustomQueuingStrategy {
 }
 
 export interface ReadableStream extends AsyncIterable<unknown> {
+  readonly locked: boolean
+
   getReader(): ReadableStreamDefaultReader
   cancel(reason?: unknown): Promise<void>
-  pipeTo(destination: Writable): Promise<void>
+  tee(): [ReadableStream, ReadableStream]
+  pipeTo(destination: WritableStream): Promise<void>
 }
 
 export class ReadableStream {
@@ -66,3 +71,47 @@ export class ByteLengthQueuingStrategy extends QueuingStrategy {}
 export function isReadableStream(value: unknown): value is ReadableStream
 
 export function isReadableStreamDisturbed(stream: ReadableStream): boolean
+
+export interface WritableStreamDefaultWriter {
+  readonly desiredSize: number
+  readonly closed: Promise<void>
+  readonly ready: Promise<void>
+
+  write(chunk: unknown): Promise<void>
+  releaseLock(): void
+  close(): Promise<void>
+  abort(reason?: unknown): Promise<void>
+}
+
+export class WritableStreamDefaultWriter {
+  constructor(stream: WritableStream)
+}
+
+export interface WritableStreamDefaultController {
+  error(err?: unknown): void
+}
+
+export class WritableStreamDefaultController {
+  constructor(stream: WritableStream)
+}
+
+export interface UnderlyingSink<S extends WritableStream = WritableStream> {
+  start?(this: S, controller: WritableStreamDefaultController): void
+  write?(this: S, chunk: unknown, controller: WritableStreamDefaultController): void
+  close?(this: S): void
+  abort?(this: S, reason?: unknown): void
+}
+
+export interface WritableStream extends AsyncIterable<unknown> {
+  readonly locked: boolean
+
+  getWriter(): WritableStreamDefaultWriter
+  abort(reason?: unknown): Promise<void>
+  close(): Promise<void>
+}
+
+export class WritableStream {
+  constructor(underlyingSink?: UnderlyingSink, queuingStrategy?: CustomQueuingStrategy)
+}
+
+export function isWritableStream(value: unknown): value is WritableStream
